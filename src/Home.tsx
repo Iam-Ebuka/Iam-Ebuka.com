9d5c5b6b6ea21c6444571cb9d40e06454d412c8e
Iam-Ebuka <ebukagrizz@gmail.com>
2024-02-05 21:24:33 +0100

    working on the homepage

--- src/Home.tsx
+++ src/Home.tsx
@@ -3,7 +3,9 @@ import React from 'react'
 const Home: React.FC = () => {
   return (
     <div>
-        <div className="bg-[#0e0b0b] h-screen w-screen"> Home</div>
+        <div className="bg-[#0e0b0b] h-screen w-screen"> 
+            <h1 className='text-[#a1a0a0] font-extrabold text-[30px]'>FRONT-END DEVELOPER</h1>
+        </div>
     </div>
   )
 }
