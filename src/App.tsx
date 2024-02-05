ea98a96148d252476ca35d9e49ea4d13bb17e6bf
Iam-Ebuka <ebukagrizz@gmail.com>
2024-02-05 21:17:15 +0100

    background done

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
-
-
+import  Home  from "./Home.tsx"
 
 
 const App: React.FC = () => {
 
   return (
     <>
-      <div className="bg-[#0e0b0b]">
-        <div></div>
+      <div className="bg-[#0e0b0b] h-full w-full">
+       <Home />
       </div>
     </>
   )
---
+++ src/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home: React.FC = () => {
+  return (
+    <div>
+        <div className="bg-[#0e0b0b] h-screen w-screen"> Home</div>
+    </div>
+  )
+}
+
+export default Home
