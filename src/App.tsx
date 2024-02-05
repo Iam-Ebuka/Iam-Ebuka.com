b74fecb8e46dcbec4c97ab44a835887f6d7d3543
Iam-Ebuka <ebukagrizz@gmail.com>
2024-02-05 21:00:26 +0100

    work is going on

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,9 @@ const App: React.FC = () => {
 
   return (
     <>
-      <div className="bg-[#5f5f5f]"></div>
+      <div className="bg-[#5f5f5f]">
+        <div></div>
+      </div>
     </>
   )
 }
