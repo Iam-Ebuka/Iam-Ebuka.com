84a991db82f6b07817949be80dab0cb330f9e5d9
Iam-Ebuka <ebukagrizz@gmail.com>
2024-02-06 23:25:04 +0100

    addition to home

--- src/Home.tsx
+++ src/Home.tsx
@@ -4,7 +4,8 @@ const Home: React.FC = () => {
   return (
     <div>
         <div className="bg-[#0e0b0b] h-screen w-screen"> 
-            <h1 className='text-[#fff] font-extrabold text-[30px]'>FRONT-END <span className='animate-[wiggle_1s_ease-in-out_infinite]'>❤️</span> 👋🏽</h1>
+            <h1 className='text-[#fff] font-extrabold text-[30px]'>FRONT-END DEVELOPER</h1>
+            <h2>Let's build from here</h2>
         </div>
     </div>
   )
