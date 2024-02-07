8c2aa01c45fd2ff7fc9fbae3471d79b5454864cb
Iam-Ebuka <ebukagrizz@gmail.com>
2024-02-07 01:20:20 +0100

    done the error 404 page

--- src/Routes/ErrorPage.tsx
+++ src/Routes/ErrorPage.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
 
-const ErrorPage = () => {
+const ErrorPage: React.FC = () => {
   return (
-    <div>ErrorPage</div>
+    <div className='bg-black h-screen w-screen flex flex-col justify-center items-center'>
+        <h1 className='text-white text-[90px] animate-bounce'>404</h1>
+        <p className='text-white'>Check the url and make corrections</p>
+    </div>
   )
 }
 
