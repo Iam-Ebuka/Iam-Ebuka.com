a027356141b0e388a62ef5b3eef6a844a9d1cd61
Iam-Ebuka <ebukagrizz@gmail.com>
2024-02-21 16:05:09 +0100

    center the projects

--- src/Projects.tsx
+++ src/Projects.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Projects: React.FC = () => {
   return (
     <div>
-        <div>Projects</div>
+        <div className="text-center">Projects</div>
     </div>
   )
 }
